a8a388777aec0b3d45e146f8e0b83e1cdd4f427f
Son Quach <sonqhb@gmail.com>
2020-07-30 06:26:28 +0700

    Learn variable and datatypes

---
+++ 3-variable/ex3-1.js
@@ -0,0 +1,8 @@
+// Khai báo 2 biến x, y nhận giá trị số bất kì
+// Khai báo các biến sum, sub, mul, div lần lượt gán giá trị tổng, hiệu, tích, thương của x, y cho từng biến.
+var x = 2;
+var y = 3;
+var sum = x + y;
+var sub = x - y;
+var mul = x * y;
+var div = x / y;
---
+++ 4-Datatypes/index.js
@@ -0,0 +1,36 @@
+/*
+Data types:
+Primitive types:
+	- Number
+	- String
+	- Boolean
+Special types:
+	- undefined
+	- null
+Refirence types:
+	- Array
+	- Object
+*/
+// Number
+var a1 = 2;
+var a2 = 0.4;
+// String
+var str = 'Hello world!';
+var str2 = "Hello, world.";
+console.log(str2);
+var str3 = 'Hello, I\'m Son.';
+console.log(str3);
+//escape a character
+
+//Boolean (true, false)
+var isHidden = true;
+console.log(isHidden);
+var isMale = false;
+
+// undefined
+var a;
+console.log(a);
+a = 100// var a = 100
+// null
+a = null
+console.log(a);
